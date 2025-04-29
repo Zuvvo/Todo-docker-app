d742490c33a5cd5feaf160e47170b5c9068db26e
Zuvvo <Zuvvo4@gmail.com>
2025-04-29 22:43:19 +0200

    auto migration on start

--- App/Program.cs
+++ App/Program.cs
@@ -20,6 +20,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
