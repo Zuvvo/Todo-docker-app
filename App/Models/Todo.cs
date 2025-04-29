64c9218e1fa96e0682941dc89039463fcb9aa091
Zuvvo <Zuvvo4@gmail.com>
2025-04-29 19:25:29 +0200

    init template docker todo app

---
+++ App/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public float Progress { get; set; } = 0;
+    }
+}
